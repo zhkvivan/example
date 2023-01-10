14453c0b7129ffdcc278c7dc68dcc7b621a13086
Ivan Zhukov <65277182+zhkvivan@users.noreply.github.com>
2023-01-10 14:20:24 +0000

    add ts and xml files

---
+++ ts-example.ts
@@ -0,0 +1,9 @@
+const a = 10;
+
+const notFormattedFunc = () => {
+  console.log('i am not formatted but commited this way');
+};
+
+const formattedFunc = () => {
+  console.log('I am the formatted one');
+};
